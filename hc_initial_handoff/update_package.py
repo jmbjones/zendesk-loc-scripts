2eaf36b0d47921ebeed75c8070b2a3c65b9f525a
chucknado <cnadeau@zendesk.com>
2014-05-08 10:32:57 -0700

    Add update package script; fix list

--- hc_initial_handoff/update_package.py
+++ hc_initial_handoff/update_package.py
@@ -5,11 +5,10 @@ import json
 import requests
 from bs4 import BeautifulSoup, Comment
 
-
 def main():
-    subdomain = 'your_subdomain'            # setting
-    email = 'your_email'                    # setting
-    password = 'your_zd_password'           # setting
+    subdomain = 'your_subdomain'           # setting
+    email = 'your_email'                   # setting
+    password = 'your_zd_password'          # setting
     session = requests.Session()
     session.auth = (email, password)
     session.headers = {'Content-Type': 'application/json'}
@@ -23,14 +22,13 @@ def main():
         for file in files:
             print('Reading {}/{} ...'.format(locale, file[path_len:]))
             payload = create_payload(locale, file)
-            url = 'https://{}.zendesk.com/api/v2/help_center/articles/{}/translations.json'.format(subdomain, file[path_len:-5])
-            post_translation(payload, url, session)
+            url = 'https://{}.zendesk.com/api/v2/help_center/articles/{}/translations/{}.json'.format(subdomain, file[path_len:-5], locale)
+            put_translation(payload, url, session)
             print('https://{}.zendesk.com/hc/{}/articles/{}.html\n'.format(subdomain, locale, file[path_len:-5]))
         print('\n-----\n')
 
 #-------------------------------------------------------------------------------
 # FUNCTIONS
-
 
 def create_payload(locale, file):
     with open(file, mode='r', encoding='utf-8') as f:
@@ -46,16 +44,15 @@ def create_payload(locale, file):
         comment.extract()
 
     # Package the payload in a dict and JSON-encode it
-    data = {'translation': {'locale': locale, 'title': title, 'body': str(tree)}}
+    data = {'translation': {'title': title, 'body': str(tree)}}
     return json.dumps(data)
 
-
-def post_translation(payload, url, session):
-    response = session.post(url, data=payload)
-    if response.status_code != 201:
-        print('Status:', response.status_code, 'Problem with the post request. Exiting.')
+def put_translation(payload, url, session):
+    response = session.put(url, data=payload)
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Problem with the put request. Exiting.')
         exit()
-    print('Translation created successfully.')
+    print('Translation updated successfully.')
 
 #-------------------------------------------------------------------------------
 # RUN SCRIPT
